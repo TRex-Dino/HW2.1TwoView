f251216c70b7ad47209487ed4e4ddf881c03c359
TRex-Dino <dmitrysinkov6@gmail.com>
2021-03-23 05:29:25 +0300

    Initial Commit

---
+++ HW2.1TwoView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1TwoView
+//
+//  Created by Dmitry on 23.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
